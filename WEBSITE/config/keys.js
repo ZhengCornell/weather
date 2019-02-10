077098ec959ba2b25a51c5dfa001e57c65f614ca
ZhengCornell <32581205+ZhengCornell@users.noreply.github.com>
2019-02-10 16:18:34 -0500

    Delete keys.js

--- WEBSITE/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sendGridKey:
-    "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",
-  // weatherKey: "fd02396dfb98477382e4be683f60d3d4"
-  weatherKey: "e6fcc42579b44171adc3e307c6fb8eff"
-};
